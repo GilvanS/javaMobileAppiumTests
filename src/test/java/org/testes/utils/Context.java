d906a2c52b9d88971aa7dedd6e5934c72ec26171
GilvanJSousa <gilvan.sousa@4win.com.br>
2025-06-26 13:19:42 -0300

    up

--- src/test/java/org/testes/utils/Context.java
+++ src/test/java/org/testes/utils/Context.java
@@ -2,13 +2,11 @@ package org.testes.utils;
 
 import io.appium.java_client.AppiumDriver;
 import org.testes.driver.actions.PageBaseActions;
-import org.testes.paginas.login.LoginPage;
 
 public class Context {
 
 	private static final ThreadLocal<PageBaseActions> acoes = new ThreadLocal<PageBaseActions>();
 	private static final ThreadLocal<AppiumDriver> driver = new ThreadLocal<AppiumDriver>();
-//	private static final ThreadLocal<LoginPage> loginPage = new ThreadLocal<LoginPage>();
 
 	public static PageBaseActions acoes() {
 		return acoes.get();
@@ -26,6 +24,5 @@ public class Context {
 	public static void remove() {
 		driver.remove();
 		acoes.remove();
-//		loginPage.remove();
 	}
 }
