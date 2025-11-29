7ab43a7fca4eb717fc8e519ba7d51bd3be210769
GilvanJSSousa <gillvanjs@gmail.com>
2025-11-29 14:16:55 -0300

    feature/home inicio

--- src/test/java/org/testes/paginas/home/HomeSteps.java
+++ src/test/java/org/testes/paginas/home/HomeSteps.java
@@ -9,12 +9,31 @@ public class HomeSteps {
     
     @Given("que estou na pagina inicial")
     public void queEstouNaPaginaInicial() {
-        HomeActions.validarOTextoOla();
     }
 
     @When("valido o titulo da pagina como {string}")
     public void validoOTituloDaPaginaComoOlá(String arg0) {
-        HomeActions.clicarNoBotaoEntreNaSuaConta();
+        HomeActions.validarOTextoOla();
     }
 
+
+    @And("clico no botão Entre na conta na {string}")
+    public void clicoNoBotãoEntreNaContaNaTelaInicial() {
+    }
+
+    @And("preencho o campo CPF com {string} {string}")
+    public void preenchoOCampoCPFComTelaInicial(int arg0) {
+    }
+
+    @And("preencho o campo Senha com {string} {string}")
+    public void preenchoOCampoSenhaComAdminTelaInicial(int arg0) {
+    }
+
+    @And("clico no botao {string} {string}")
+    public void clicoNoBotaoEntrarTelaInicial() {
+    }
+
+    @Then("devo ver a mensagem de boas vindas {string} {string}")
+    public void devoVerAMensagemDeBoasVindasAcessoMinhaContaTelaHome() {
+    }
 }
